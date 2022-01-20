3322551b383065cf3238b30f4e5405b2164e101c
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-20 15:17:06 +0100

    adding tz, and rerunning consul, turning off asg
    Former-commit-id: f21cbb71e547794ddc223244a606b6c1cd5142e1

---
+++ scripts/amiexports.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+aws ec2 describe-images --owners self --filters "Name=tag:CreatedBy,Values=ops" \
+	--query 'Images[*].[ImageId,Tags[?Key==`Name`].Value | [0]]' --output text \
+	| while read ami name; do
+	name="${name^^}AMI"
+	echo "export $name='$ami'"
+
+done
+
+
