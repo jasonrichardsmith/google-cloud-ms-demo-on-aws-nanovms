d5c493c593533466009a0e23032694f470087056
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-21 13:21:52 +0100

    fixing logger bin ref and step names
    Former-commit-id: 78b6845df50d580d49d3a7d455c09ebd4404e8a2

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd images/logger
 cat <<< $( jq --arg bucket "$bucket" '.CloudConfig.BucketName = $bucket' config.json) > config.json
-/root/.ops/bin/ops image create logger -i logger -c config.json --show-debug -t aws
+/root/.ops/bin/ops image create syslog-cloudwatch-bridge -i logger -c config.json --show-debug -t aws
