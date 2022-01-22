f40346f48a1216f71d0fb76d00d48de5a551c269
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-22 09:54:41 +0100

    trying new image
    Former-commit-id: f770f439d827d2dd02e2a0f9c20f482d0b171e1b

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -5,8 +5,8 @@ mkdir -p ~/.ops/local_packages
 mv consul_1.11.2 ~/.ops/local_packages/
 /root/.ops/bin/ops pkg add consul_1.11.2 --name consul_1.11.2
 cat <<< $(jq --arg node "consul1" '.node_name = $node' consul.config) > consul.config
-/root/.ops/bin/ops image create --package consul_1.11.2 -l -i consul1 -c config.json --show-debug -t aws
+ops image create --package consul_1.11.2 -l -i consul1 -c config.json --show-debug -t aws
 cat <<< $(jq --arg node "consul2" '.node_name = $node' consul.config) > consul.config
-/root/.ops/bin/ops image create --package consul_1.11.2 -l -i consul2 -c config.json --show-debug -t aws
+ops image create --package consul_1.11.2 -l -i consul2 -c config.json --show-debug -t aws
 cat <<< $(jq --arg node "consul3" '.node_name = $node' consul.config) > consul.config
-/root/.ops/bin/ops image create --package consul_1.11.2 -l -i consul3 -c config.json --show-debug -t aws
+ops image create --package consul_1.11.2 -l -i consul3 -c config.json --show-debug -t aws
