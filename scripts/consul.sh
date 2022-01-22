b2cbac55a0d725630323f184150b8c2a3a53ac8b
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-22 10:04:58 +0100

    troubleshooting missing dir
    Former-commit-id: 39f9728525867bbe00e1d1ad14897bbe7a26c3c0

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -2,11 +2,13 @@
 cd images/consul
 ls -al
 pwd
-exit
 cat <<< $(jq --arg bucket "$bucket" '.CloudConfig.BucketName = $bucket' config.json) > config.json
 mkdir -p ~/.ops/local_packages
-mv consul_1.11.2 ~/.ops/local_packages/
-/root/.ops/bin/ops pkg add consul_1.11.2 --name consul_1.11.2
+echo "moving consul package"
+mv ./consul_1.11.2 ~/.ops/local_packages/
+echo "adding package to ops"
+ops pkg add consul_1.11.2 --name consul_1.11.2
+exit
 cat <<< $(jq --arg node "consul1" '.node_name = $node' consul.config) > consul.config
 ops image create --package consul_1.11.2 -l -i consul1 -c config.json --show-debug -t aws
 cat <<< $(jq --arg node "consul2" '.node_name = $node' consul.config) > consul.config
