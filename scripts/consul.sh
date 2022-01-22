0c7cf7f9323d09f0146576dcc0d3a15ef4d2da4c
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-22 10:55:51 +0100

    trying to fix ops image
    Former-commit-id: 0ae1ddeb499867f386a788dd021d521e9101d99c

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -2,7 +2,6 @@
 cd images/consul
 cat <<< $(jq --arg bucket "$bucket" '.CloudConfig.BucketName = $bucket' config.json) > config.json
 ops pkg add consul_1.11.2 --name consul_1.11.2
-exit
 cat <<< $(jq --arg node "consul1" '.node_name = $node' consul.config) > consul.config
 ops image create --package consul_1.11.2 -l -i consul1 -c config.json --show-debug -t aws
 cat <<< $(jq --arg node "consul2" '.node_name = $node' consul.config) > consul.config
