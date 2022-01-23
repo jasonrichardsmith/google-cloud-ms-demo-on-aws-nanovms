598d2982abd3d1462d4edea3ca339706b330e5a3
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-23 09:08:33 +0100

    pythonscript test
    Former-commit-id: 1b5308557ae1d4f9ff7589c23b50af73c7131a8e

--- scripts/emailservice.sh
+++ scripts/emailservice.sh
@@ -3,6 +3,5 @@ jq --arg bucket "$bucket" '.CloudConfig.BucketName = $bucket' images/emailservic
 	> microservices-demo/src/emailservice/config.json
 cp -rf images/emailservice/etc microservices-demo/src/emailservice/
 cp -rf images/emailservice/usr microservices-demo/src/emailservice/
-cp -rf images/emailservice/templates microservices-demo/src/emailservice/
 cd microservices-demo/src/emailservice
 ops image create --package python_3.8.6 -i emailservice -c config.json --show-debug -t aws
