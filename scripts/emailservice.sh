5922d34092dfbe07544717c0785fd9c7805a91d6
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-23 08:44:00 +0100

    slimming email build and removing product is done
    Former-commit-id: f60b2c97ed115ab56aee91419cb847ab11bb087f

--- scripts/emailservice.sh
+++ scripts/emailservice.sh
@@ -2,5 +2,7 @@
 jq --arg bucket "$bucket" '.CloudConfig.BucketName = $bucket' images/emailservice/config.json \
 	> microservices-demo/src/emailservice/config.json
 cp -rf images/emailservice/etc microservices-demo/src/emailservice/
+cp -rf images/emailservice/usr microservices-demo/src/emailservice/
+cp -rf images/emailservice/templates microservices-demo/src/emailservice/
 cd microservices-demo/src/emailservice
 ops image create --package python_3.8.6 -i emailservice -c config.json --show-debug -t aws
