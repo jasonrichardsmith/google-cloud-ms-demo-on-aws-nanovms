95900c5c0f383eaac2e254bb6644ebd71763d0e2
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-23 08:57:02 +0100

    pythonscript test
    Former-commit-id: 3d0de5e007a2cfa14860cc442a90c8e3f2f8fbb1

--- scripts/emailservicepythonbuild.sh
+++ scripts/emailservicepythonbuild.sh
@@ -2,9 +2,9 @@
 cp images/emailservice/logger.patch microservices-demo/src/emailservice/
 cd microservices-demo/src/emailservice
 patch logger.py logger.patch
-cat logger.py
 python3 -m venv .venv --prompt nanovm
 source .venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 deactivate
+ls -al .venv
