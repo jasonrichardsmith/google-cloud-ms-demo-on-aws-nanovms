fefb875ebfdcb5cef52694ffc918d54533a37279
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-26 16:02:15 +0100

    downloading consul versis checking in
    Former-commit-id: 06c15c020eb86ea91584740d933d5f067ce773e4

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd images/consul
+curl -o consul.zip https://releases.hashicorp.com/consul/1.11.2/consul_1.11.2_linux_amd64.zip
+unzip consul.zip
+mv consul consul_1.11.2/consul
+rm consul.zip
 cat <<< $(jq --arg bucket "$bucket" '.CloudConfig.BucketName = $bucket' config.json) > config.json
 ops pkg add consul_1.11.2 --name consul_1.11.2
 cat <<< $(jq --arg node "consul1" '.node_name = $node' consul.config) > consul.config
