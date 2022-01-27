8d7e1c275d889b986f3593cb9699c3b8a949993d
jasonrichardsmith <jasonrichardsmith@gmail.com>
2022-01-27 16:36:53 +0100

    fixed frontend snapshot, cleanuing up docker builds

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-docker build -t ${1}build -f images/${1}/Dockerfile .
-docker run --env bucket=$BUCKET -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID ${1}build
+docker build --no-cache -t ${1}build -f images/${1}/Dockerfile .
+docker run --rm --env bucket=$BUCKET -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID ${1}build
+docker image rm ${1}build
